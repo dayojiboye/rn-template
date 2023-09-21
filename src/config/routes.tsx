643982304d422097f906ff9a17553caf7d76ee14
dayojiboye <my@personal>
2023-09-21 20:58:02 +0100

    feat: updates

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,13 +1,21 @@
 import React from "react";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createStackNavigator } from "@react-navigation/stack";
 import Home from "../screens/Home";
 import { RootStackParamList } from "../types";
 
-const Stack = createNativeStackNavigator<RootStackParamList>();
+const Stack = createStackNavigator<RootStackParamList>();
 
 export default function AppRoutes() {
 	return (
-		<Stack.Navigator screenOptions={{ headerShown: false, gestureEnabled: true }}>
+		<Stack.Navigator
+			screenOptions={{
+				headerBackTitleVisible: false,
+				headerTitleAlign: "center",
+				headerShadowVisible: false,
+				headerMode: "screen",
+				headerShown: false,
+			}}
+		>
 			<Stack.Screen name="Home" component={Home} />
 		</Stack.Navigator>
 	);
